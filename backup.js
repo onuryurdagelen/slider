e23029919180ab2e7d09583cd5510b7a4a190cff
DESKTOP-OKIVNDG\ONUR-PC <yurdagelenonur7@gmail.com>
2022-10-06 02:40:23 +0300

    güncelleme 02.40 6.10.2022

--- backup.js
+++ backup.js
@@ -24,43 +24,9 @@ leftArrow.addEventListener("click",()=>{
 
 function showSlides(start) {
     
-    var slides = document.getElementsByClassName("card");
-    //Left arrow
 
-    var startSlide = document.getElementsByClassName("card")[start];
-    console.log("start Slide: ",startSlide);
-
-    var arr = Array.prototype.slice.call( slides )
-    console.log("arr length: ",arr.length);
-
-    var slicedArr = arr.slice(start,arr.length);
-    container.style.overflow = "visible";
-
-    container.style.transform = `translateX(-${startSlide.clientWidth  * start}px)`;
-    console.log(startSlide.clientWidth  * start);
-    
-    let increasedTransform = startSlide.clientWidth  * start;
-    if (start == 0) {
-        container.style.transform = `translateX(-${start}px)`;
-        leftArrow.style.display = "none";
-        return;
-    }
-    if (slicedArr.length < 4) {
-        console.log(slicedArr.length);
-        clg
-        // console.log((start - slicedArr.length) * startSlide.clientWidth);
-        // console.log(startSlide.clientWidth  * start - slicedArr.length * startSlide.clientWidth);
-        // console.log(slicedArr.length * startSlide.clientWidth);
-         container.style.transform = `translateX(-${increasedTransform - (window.screen.width - (arr.length % start) * startSlide.clientWidth)}px)`;
-         console.log( ((arr.length % start) * startSlide.clientWidth));
-         rightArrow.style.display = "none";
-         console.log("**************************");
-        return;
-        
-    }
+  
    
-    leftArrow.style.display = "block";
-    rightArrow.style.display = "block";
 
     
     // console.log(startSlide.clientWidth  * start);
